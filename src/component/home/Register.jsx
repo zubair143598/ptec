15a4828912eb2efdbaa7481b8298ea8191d139ab
Muhammad Zubair <md.zubair143598@gmail.com>
2025-08-15 00:33:09 +0500

    qr code on certificate

--- src/component/home/Register.jsx
+++ src/component/home/Register.jsx
@@ -5,7 +5,9 @@ const Register = () => {
   return (
     <div className=' px-4 md:px-auto py-5 flex justify-center flex-col items-center bg-[#EEEEEE]'>
       <h3 className='md:text-[22px] text-[18px] font-bold uppercase text-[#666666]'>Register Government Of Punjab</h3>
-      <Image src="/register.png" alt="" width={250} height={214} /> 
+      <div className='w-[260px] md:w-[550px] mx-auto my-3'>
+      <Image src="/newlogo.png" alt="" layout='responsive' width={250} height={214} /> 
+      </div>
       <h3 className='text-[18px] md:text-[22px] font-bold uppercase text-[#666666]'>Project</h3>
       <h3 className='text-[18px] md:text-[22px] font-bold uppercase text-[#666666] text-center'>Pakistan Health Sciences Council & Skill Organization</h3>
     </div>
